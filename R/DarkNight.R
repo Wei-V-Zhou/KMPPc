5921575ac6b7a949959ccc5d2a82c1fd4864f526
Wei-V-Zhou <zw199709@126.com>
2020-06-01 17:59:22 +0800

    Update DarkNight.R

--- R/DarkNight.R
+++ R/DarkNight.R
@@ -1,4 +1,18 @@
 if (!requireNamespace("cBioPortalData", quietly = TRUE))
   BiocManager::install("cBioPortalData")
 
+# setwd("C:/Users/asus1/AppData/Local/Temp/RtmpGsLI18/")
+# filename <- dir()
+# for (i in 1: length(filename)) {
+#   install.packages(filename[i], repos = NULL, type = "win.binary")
+# }
+
 library("cBioPortalData")
+cbio <- cBioPortal()
+
+gbm <- cBioPortalData(api = cbio, by = "hugoGeneSymbol", studyId = "gbm_tcga",
+                      genePanelId = "IMPACT341",
+                      molecularProfileIds = c("gbm_tcga_rppa", "gbm_tcga_mrna")
+)
+
+laml <- cBioDataPack("laml_tcga")
