a48a174e086234820c03abf9491a0d2b9a962a9f
Wei-V-Zhou <zw199709@126.com>
2020-06-03 14:31:06 +0800

    Update kmplot.R

--- R/kmplot.R
+++ R/kmplot.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 gc()
 
-kmplot <- function(data = NULL, plotType = Typical) {
+kmplot <- function(data = NULL, plotType = "typical") {
   
   ## 0. prepare environment and load libraries
   # rm(list = ls())
@@ -26,41 +26,62 @@ kmplot <- function(data = NULL, plotType = Typical) {
     dat2 <<- dat2
   } else {
     print("Please guarantee your file is dat2!")
-    cat("***Notation: Or you will load the kmplotdata.Rdata on your own!")
+    cat("***Notation: Or you will load the kmplotdata.Rdata on your own!\n")
   }
   
   # survival plot
+  # choose the right status number
   factor <- names(table(dat2[ , 2]))
-  my.surv <<- Surv(dat2[ , 1], dat2[ , 2] == 'Recurred/Progressed')
+  for (i in 1:length(factor)) {
+    cat(c(i, ": ", factor[i], "\n"))
+  }
+  if(interactive()){
+    repeat{
+      ANSWER <- readline("Please input the STATUS number (Progress or Dead): ")
+      num <- as.numeric(ANSWER)
+      if(is.na(num) || num <= 0 || num > length(factor)){
+        print("Please type right number format!")
+      } else if (num > 0 && num <= length(factor)) {
+        break;
+      }
+    }
+    STATUS <- factor[num]
+  }
+  # survival fit
+  my.surv <<- Surv(dat2[ , 1], dat2[ , 2] == STATUS)
   kmfit <<- survfit(my.surv~group, data = dat2)
-  plot(kmfit, col = c("red", "blue"))
-  img <- ggsurvplot(kmfit, palette = c("#E7B800","#2E9FDF"),
-                    conf.int = TRUE, pval = TRUE, xlab = "Time / Month",
-                    ggtheme = theme_light(), risk.table = TRUE, ncensor.plot = TRUE)
-  print(img)
-  label <- data.frame(kmfit$strata)
-  labs <- cbind(rownames(label), label)
-  colnames(labs) <- c("gene_group", "n")
-  gene_high <- paste0(labs[1, 1] , "\n (n = ", labs[1, 2], ")")
-  gene_low  <- paste0(labs[2, 1] , "\n (n = ", labs[2, 2], ")")
-  names(kmfit$strata) <- c(gene_high, gene_low)
-  g <- theme_survminer(font.main = c(16, "bold", "darkblue"),
-                       font.x = c(14, "bold", "black"),
-                       font.y = c(14, "bold", "black"),
-                       font.tickslab = c(14, "bold", "black")) +
-    theme(axis.line.x = element_line(size = 1),
-          axis.line.y = element_line(size = 1),
-          axis.ticks.x = element_line(size = 1),
-          axis.ticks.y = element_line(size = 1),
-          plot.title = element_text(hjust = 0.5),
-          legend.title = element_blank(),
-          legend.direction = "horizontal",
-          legend.text = element_text(size = 12, face = "bold"))
-  p <- ggsurvplot(kmfit, palette = c("red","blue"),
-                  conf.int = F, pval = T, pval.method = T, surv.scale = "percent",
-                  xlab ="Months" , ylab = "% Disease-free", 
-                  ggtheme = g, risk.table = F, ncensor.plot = F) 
-  
-  print(p)
-  
+  if (plotType == "simple") {
+    plot(kmfit, col = c("red", "blue"))
+  } else if (plotType == "specified") {
+    img <- ggsurvplot(kmfit, palette = c("#E7B800","#2E9FDF"),
+                      conf.int = TRUE, pval = TRUE, xlab = "Time / Month",
+                      ggtheme = theme_light(), risk.table = TRUE, ncensor.plot = TRUE)
+    print(img)
+  } else if (plotType == "typical") {
+    label <- data.frame(kmfit$strata)
+    labs <- cbind(rownames(label), label)
+    colnames(labs) <- c("gene_group", "n")
+    gene_high <- paste0(labs[1, 1] , "\n (n = ", labs[1, 2], ")")
+    gene_low  <- paste0(labs[2, 1] , "\n (n = ", labs[2, 2], ")")
+    names(kmfit$strata) <- c(gene_high, gene_low)
+    g <- theme_survminer(font.main = c(16, "bold", "darkblue"),
+                         font.x = c(14, "bold", "black"),
+                         font.y = c(14, "bold", "black"),
+                         font.tickslab = c(14, "bold", "black")) +
+      theme(axis.line.x = element_line(size = 1),
+            axis.line.y = element_line(size = 1),
+            axis.ticks.x = element_line(size = 1),
+            axis.ticks.y = element_line(size = 1),
+            plot.title = element_text(hjust = 0.5),
+            legend.title = element_blank(),
+            legend.direction = "horizontal",
+            legend.text = element_text(size = 12, face = "bold"))
+    p <- ggsurvplot(kmfit, palette = c("red","blue"),
+                    conf.int = F, pval = T, pval.method = T, surv.scale = "percent",
+                    xlab ="Months" , ylab = "% Disease-free", 
+                    ggtheme = g, risk.table = F, ncensor.plot = F) 
+    
+    print(p)
+  }
+
 }
